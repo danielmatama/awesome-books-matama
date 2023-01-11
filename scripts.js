296cb01a82937e3aeb27dc574ff9beb44adc36ea
daniel matama <danmatama2020@gmail.com>
2023-01-11 13:57:50 +0300

    Complete website

--- scripts.js
+++ scripts.js
@@ -20,29 +20,29 @@ const allBooks = new Books([]);
 
 function addToLocalStorage() {
   const stringifyArray = JSON.stringify(allBooks.bookArray);
-  localStorage.setItem("storedBooks", stringifyArray);
+  localStorage.setItem('storedBooks', stringifyArray);
 }
 
 function displayBook() {
-  const addedBooks = document.getElementById("list");
-  addedBooks.innerHTML = "";
+  const addedBooks = document.getElementById('list');
+  addedBooks.innerHTML = '';
   for (let i = 0; i < allBooks.bookArray.length; i += 1) {
-    const container = document.createElement("div");
-    container.classList.add("book");
+    const container = document.createElement('div');
+    container.classList.add('book');
     addedBooks.appendChild(container);
 
-    const container02 = document.createElement("div");
-    container02.classList.add("titleAuthor");
+    const container02 = document.createElement('div');
+    container02.classList.add('titleAuthor');
     container.appendChild(container02);
 
-    const bookDetails = document.createElement("p");
-    bookDetails.classList.add("title");
+    const bookDetails = document.createElement('p');
+    bookDetails.classList.add('title');
     bookDetails.textContent = `"${allBooks.bookArray[i].title}" by ${allBooks.bookArray[i].author}`;
     container02.appendChild(bookDetails);
 
-    const removeButton = document.createElement("button");
-    removeButton.classList.add("remove");
-    removeButton.textContent = "Remove";
+    const removeButton = document.createElement('button');
+    removeButton.classList.add('remove');
+    removeButton.textContent = 'Remove';
 
     removeButton.onclick = () => {
       allBooks.booksFilter(allBooks.bookArray[i]);
@@ -54,64 +54,50 @@ function displayBook() {
 }
 
 function getFromLocalStorage() {
-  const stringifyArray = localStorage.getItem("storedBooks");
+  const stringifyArray = localStorage.getItem('storedBooks');
   allBooks.bookArray = JSON.parse(stringifyArray);
   displayBook();
 }
 
-if (localStorage.getItem("storedBooks") == null) {
+if (localStorage.getItem('storedBooks') == null) {
   addToLocalStorage();
 } else {
   getFromLocalStorage();
 }
 
-const addBtn = document.getElementById("addButton");
-addBtn.addEventListener("click", () => {
-  const title = document.getElementById("title");
-  const author = document.getElementById("author");
+const addBtn = document.getElementById('addButton');
+addBtn.addEventListener('click', () => {
+  const title = document.getElementById('title');
+  const author = document.getElementById('author');
   allBooks.bookObj(title.value, author.value);
   addToLocalStorage();
   displayBook();
 });
 
-// eslint-disable-next-line no-unused-vars
-function displaySection(section) {
-  const sectionList = document.getElementById("list");
-  const sectionForm = document.getElementById("form");
-  const sectionContact = document.getElementById("contact");
-  const heading = document.getElementById("bookTitle");
+// ....Page contents...//
+const list = document.getElementById('list');
+const addNew = document.getElementById('addNew');
+const contactSection = document.getElementById('contact-section');
 
-  switch (section) {
-    case "list":
-      sectionList.style.display = "block";
-      sectionForm.style.display = "none";
-      sectionContact.style.display = "none";
-      heading.innerHTML = "All Awesome Books";
-      break;
+//... Nav buttons...//
+const listLink = document.getElementById('list-link');
+const addLink = document.getElementById('add-link');
+const contactLink = document.getElementById('contact-link');
 
-    case "form":
-      sectionList.style.display = "none";
-      sectionForm.style.display = "block";
-      sectionContact.style.display = "none";
-      heading.innerHTML = "Add a New Book";
-      break;
+listLink.addEventListener('click', () => {
+  list.classList.remove('hide');
+  addNew.classList.add('hide');
+  contactSection.classList.add('hide');
+});
 
-    case "contact":
-      sectionList.style.display = "none";
-      sectionForm.style.display = "none";
-      sectionContact.style.display = "block";
-      heading.innerHTML = "Contact Information";
-      break;
+addLink.addEventListener('click', () => {
+  addNew.classList.remove('hide');
+  list.classList.add('hide');
+  contactSection.classList.add('hide');
+});
 
-    default:
-      break;
-  }
-}
-
-function setDate() {
-  const date = document.getElementById("date");
-  // eslint-disable-next-line no-undef
-  const { DateTime } = luxon;
-
-  date.innerHTML = DateTime.now().toFormat("LLL dd yyyy, t");
-}
+contactLink.addEventListener('click', () => {
+  contactSection.classList.remove('hide');
+  addNew.classList.add('hide');
+  list.classList.add('hide');
+});
